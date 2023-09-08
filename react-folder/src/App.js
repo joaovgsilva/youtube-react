49a2bd57d02ded8c90bf4f20380ba02fa7d0524e
João Gomes <joaovgsilva20@gmail.com>
2023-09-08 16:25:46 -0300

    to be continued

--- react-folder/src/App.js
+++ react-folder/src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld'
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+  const nome = 'José'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> {/* maneira correta de adicionar uma "class" no React = "className" */}
+      <HelloWorld />
+      <SayMyName nome='João'/>
+      <SayMyName nome='Maria'/>
+      <SayMyName nome={nome} />
+      <Pessoa nome='João' idade='21' profissao='Programador' foto='https://via.placeholder.com/150'/>
     </div>
   );
 }
---
+++ react-folder/src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import Frase from './Frase'
+
+function HelloWorld(){
+    return (
+        <div className="">
+            <Frase />
+            <h1>meu primeiro componente c:</h1>
+            <Frase />
+        </div>
+    )
+}
+
+export default HelloWorld;
