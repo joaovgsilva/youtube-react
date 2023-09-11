d9e991920db707b8fd2fe8805f2c65c781dcfe84
João Gomes <joaovgsilva20@gmail.com>
2023-09-11 18:51:58 -0300

    adding CSS

--- react-folder/src/App.js
+++ react-folder/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = 'José'
   return (
     <div className="App"> {/* maneira correta de adicionar uma "class" no React = "className" */}
       <HelloWorld />
+      <Frase />
       <SayMyName nome='João'/>
       <SayMyName nome='Maria'/>
       <SayMyName nome={nome} />
