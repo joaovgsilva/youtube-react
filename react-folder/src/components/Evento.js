8cce3cd3d994026633aced57fae803034a776333
João Gomes <joaovgsilva20@gmail.com>
2023-09-19 17:33:17 -0300

    event by props

--- react-folder/src/components/Evento.js
+++ react-folder/src/components/Evento.js
@@ -1,13 +1,20 @@
+import Button from './event/Button'
+
 function Evento(){
 
     function meuEvento(){
-        alert('Opa, fui ativado!');
+        alert('Opa, primeiro evento ativado!');
+    }
+
+    function segundoEvento(){
+        alert('Opa, segundo evento ativado!');
     }
 
     return(
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo Evento" />
         </div>
     )
 }
