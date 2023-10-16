22963c645950bb1e2705762e46f8394482ce0ba1
João Gomes <joaovgsilva20@gmail.com>
2023-10-16 16:42:15 -0300

    creating the home

--- project/src/components/layout/Navbar.js
+++ project/src/components/layout/Navbar.js
@@ -17,13 +17,13 @@ function Navbar(){
                         <Link to='/'>Home</Link>
                     </li>
                     <li className={styles.item}>
-                        <Link to='/projects'>Projects</Link>
+                        <Link to='/projects'>Projetos</Link>
                     </li>
                     <li className={styles.item}>
-                        <Link to='/company'>Company</Link>
+                        <Link to='/company'>Empresa</Link>
                     </li>
                     <li className={styles.item}>
-                        <Link to='/contact'>Contact</Link>
+                        <Link to='/contact'>Contato</Link>
                     </li>
                 </ul>
             </Container>
